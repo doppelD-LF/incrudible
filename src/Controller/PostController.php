3cbc49360ff0947b767841c3e79c6b6ca984d96e
Daniel Diele <d.diele@leuchtfeuer.com>
2021-03-02 10:24:28 +0100

    added twig conditions for category and image display

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -79,8 +79,8 @@ class PostController extends AbstractController
      * @Route("/show/{id}", name="show")
      */
     public function show(Post $post) {
-        
-        dump($post); 
+
+        //dump($post); 
 
         return $this->render('post/show.html.twig', [
             'post' => $post
